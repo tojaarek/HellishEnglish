9ab8fe5d0d29555deef0ce6e3fcbda4dd61d9d5b
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-09-06 15:32:54 +0000

    Deploying to gh-pages from @ tojaarek/HellishEnglish@1721d2c000b4c4a5e58f4a3027c5ee45097fa040 🚀

---
+++ index.33ed2e80.js
@@ -0,0 +1,2 @@
+function checkAndAnimate(){document.querySelectorAll(".assets__description").forEach((e=>{e.getBoundingClientRect().top<=800&&e.classList.add("animate__bounceInRight")}))}checkAndAnimate(),window.addEventListener("scroll",checkAndAnimate);
+//# sourceMappingURL=index.33ed2e80.js.map
--- index.febbfdc8.js
+++
@@ -1,2 +0,0 @@
-function checkAndAnimate(){document.querySelectorAll(".assets__description").forEach((n=>{n.getBoundingClientRect().top<=800&&n.classList.add("animate__bounceInRight")}))}window.innerWidth<=450&&(checkAndAnimate(),window.addEventListener("scroll",checkAndAnimate));
-//# sourceMappingURL=index.febbfdc8.js.map
